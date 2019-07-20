602eec8bfb9ffed42c2ae1e6dfbda0e3d2e4c047
Mika88 <mikaspivak@gmail.com>
2019-07-20 10:50:28 -0700

    Create GoalsContainet component with imports from react and Goals and GoalInput components to render them

---
+++ src/containers/GoalsContainer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import GoalInput from '../components/goals/GoalInput'
+import Goals from '../components/goals/Goals'
+
+
+export default class GoalsContainer extends Component {
+
+    render() {
+      return (
+        <div>
+          <GoalInput/>
+          <Goals/>
+        </div>
+      )
+    }
+  }
+
