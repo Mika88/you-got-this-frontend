96489ea1764c22592ccf239a418cd3f602be6257
Mika88 <mikaspivak@gmail.com>
2019-07-20 12:02:00 -0700

    Add controlled form in GoalInput and define handleChange and handleSubmit functions

--- src/components/goals/GoalInput.js
+++ src/components/goals/GoalInput.js
@@ -4,12 +4,26 @@ export default class GoalInput extends Component {
     state = {
         text: ''
     }
+    
+    handleChange(event){
+      this.setState({
+        text: event.target.value
+      })
+    }
+    
+    handleSubmit(event){
+      event.preventDefault();
+    }
 
     render() {
         return (
             <div>
-           
+              <form onSubmit={this.handleSubmit}>
+                <input type="text" value={this.state.text} onChange={this.handleChange}/>
+                <input type="submit"/>
+              </form>
             </div>
         )
     }
 }
+
