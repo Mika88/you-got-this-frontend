db7b5dc038f6d38e0197d736f91a99ace7a91665
Mika88 <mikaspivak@gmail.com>
2019-07-20 11:42:35 -0700

    Add state with test attribute to GoalInput component

--- src/components/goals/GoalInput.js
+++ src/components/goals/GoalInput.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 
 export default class GoalInput extends Component {
+    state = {
+        text: ''
+    }
+
     render() {
         return (
             <div>
+           
             </div>
         )
     }
