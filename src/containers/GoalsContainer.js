084e488fb0c26b444129083af236590201530699
Mika88 <mikaspivak@gmail.com>
2019-07-20 14:16:51 -0700

    Connect GoalsContainer with store; add mapStateToProps and mapDispatchToProps with 'ADD_GOAL' action and refactor export default

--- src/containers/GoalsContainer.js
+++ src/containers/GoalsContainer.js
@@ -1,17 +1,26 @@
-import React, { Component } from 'react'
-import GoalInput from '../components/goals/GoalInput'
+import React, { Component } from 'react';
+import GoalInput from '../components/goals/GoalInput';
+import { connect } from 'react-redux';
 // import Goals from '../components/goals/Goals'
 
 
-export default class GoalsContainer extends Component {
+class GoalsContainer extends Component {
 
     render() {
       return (
         <div>
-          <GoalInput/>
+          <GoalInput addGoal={this.props.addGoal}/>
           {/* <Goals/> */}
         </div>
       )
     }
   }
+const mapStateToProps = (state) => {
+  return { goals: state.goals }
+}
 
+const mapDispatchToProps = (dispatch) => {
+  return { addGoal: text => dispatch({type: 'ADD_GOAL', text}) }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(GoalsContainer)
