313e9379dd890ccc03492aff6f1b91fecf66f093
Mika88 <mikaspivak@gmail.com>
2019-07-20 11:21:38 -0700

    Revert "Setup reducers/index.js, import combineReducers from redux, import manageGoals from manageGoal. Define rootReducer"
    This reverts commit b20c5f2387dc5228bc9b5670c63daa66d92f5973.

--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import goalsReducer from './manageGoals';
-
-const rootReducer = combineReducers({
-    goals: goalsReducer
- 
-  });
-
-export default rootReducer
