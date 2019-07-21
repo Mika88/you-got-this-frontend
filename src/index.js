e111fca4082c473527886ce7d019466d5c9c0f69
Mika88 <mikaspivak@gmail.com>
2019-07-21 11:38:08 -0700

    Import thunk, applyMiddleware and compose into index.js, and refactor createStore function to handle compose and the thunk middleware

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose} from 'redux';
 import rootReducer from './reducers/index';
+import thunk from 'redux-thunk';
 
 let store = createStore(
     rootReducer,
+    compose(
+    applyMiddleware(thunk),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
   );
 
 ReactDOM.render(
