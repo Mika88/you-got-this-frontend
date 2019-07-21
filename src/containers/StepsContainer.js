e2bbc8ec504c890d32ad337a923c738803e388dd
Mika88 <mikaspivak@gmail.com>
2019-07-21 09:48:48 -0700

    Setup StepsContainer: import and render Steps and StepInput components, connect container to store and define mapStateToProps and mapDispatchToProps

---
+++ src/containers/StepsContainer.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import StepInput from '../components/steps/StepInput';
+import { connect } from 'react-redux';
+import Steps from '../components/steps/Steps'
+
+
+class StepsContainer extends Component {
+
+    render() {
+      return (
+        <div>
+          <StepInput addGoal={this.props.addGoal}/>
+          <Steps steps={this.props.steps}/>
+        </div>
+      )
+    }
+  }
+const mapStateToProps = (state) => {
+  return { steps: state.steps }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+   addStep: text => dispatch({type: 'ADD_STEP', text}) 
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(StepsContainer)
