ad9706fd9eb98c1e61438cb9a3125eb1e30650e5
Mika88 <mikaspivak@gmail.com>
2019-07-22 13:28:44 -0700

    commant out <ul> content in Goals component

--- src/components/goals/Goals.js
+++ src/components/goals/Goals.js
@@ -3,7 +3,7 @@ import Goal from './Goal';
 
 const Goals = (props) =>
     <ul>
-        { props.goals.map((goal, id) => <Goal key={id} goal={goal}/>) }
+        {/* { props.goals.map((goal, id) => <Goal key={id} goal={goal}/>) } */}
     </ul>
         
 export default Goals
