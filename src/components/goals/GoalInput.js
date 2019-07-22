0389f621ec25d90d11ca7aecc474ec0d6a496a1b
Mika88 <mikaspivak@gmail.com>
2019-07-22 15:21:45 -0700

    Create a postGoal function that handles a post fetch request to the rails api with the state object. Call function in handleOnSubmit

--- src/components/goals/GoalInput.js
+++ src/components/goals/GoalInput.js
@@ -7,6 +7,17 @@ export default class GoalInput extends Component {
         deadline : ''
     }
     
+    postGoal(){
+      fetch('http://localhost:3001/api/v1/goals', {
+        method: 'post',
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify(this.state) 
+      })
+        .then(res => res.json())
+        .catch(error => console.log("Error:", error))
+        .then(response => console.log("Success:", response));
+    }  
+
     handleOnChange(event){
       this.setState({
         [event.target.name] : event.target.value,
@@ -15,7 +26,7 @@ export default class GoalInput extends Component {
     
     handleOnSubmit(event){
       event.preventDefault();
-      // this.props.addGoal(this.state.text);
+      this.postGoal()
       this.setState({
           [event.target.name]: ''
       })
