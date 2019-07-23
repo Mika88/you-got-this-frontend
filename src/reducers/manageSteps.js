b2ac418f298a5b78b7f6077f647315875d6b25ad
Mika88 <mikaspivak@gmail.com>
2019-07-23 12:03:33 -0700

    Refactor stepsReducer to have 'ADD_STEP' and 'LOADING' actions

--- src/reducers/manageSteps.js
+++ src/reducers/manageSteps.js
@@ -1,10 +1,21 @@
-function stepsReducer(state = [], action) {
+function stepsReducer(state = {
+  steps: [],
+  loading: false }, action) {
     switch(action.type) {
-        case "ADD_STEP":
-          return [...state, action.text];
-        default:
-          return state;
-    }
+      case 'LOADING':
+      return {
+        ...state, 
+          loading: true
+        }
+      case 'ADD_GOAL':
+        return {
+          ...state,
+          loading: false,
+          sreps: [...state.steps, action.step]
+        }
+      default:
+        return state;
+      }
   }
-  
-  export default stepsReducer;
+
+export default stepsReducer;
