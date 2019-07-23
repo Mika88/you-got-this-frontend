ba1a4f5e2cd73eef3dfb9b338a10cbd02843b0ab
Mika88 <mikaspivak@gmail.com>
2019-07-22 17:18:18 -0700

    Add componentDIdMount function to GoalsContainer, and log out the list of goal objects in actions/goals to test

--- src/actions/goals.js
+++ src/actions/goals.js
@@ -3,6 +3,6 @@ export function fetchGoals() {
       dispatch({ type: 'LOADING' });
       return fetch('http://localhost:3001/api/v1/goals')
         .then(response => response.json())
-        .then(goals => dispatch({ type: 'FETCH_GOALS', goals }));
+        .then(goals => console.log(goals));
     };
   }
--- src/containers/GoalsContainer.js
+++ src/containers/GoalsContainer.js
@@ -2,25 +2,29 @@ import React, { Component } from 'react';
 import GoalInput from '../components/goals/GoalInput';
 import { connect } from 'react-redux';
 import Goals from '../components/goals/Goals'
-import {fetchGoals} from './actions/goals'
+import {fetchGoals} from '../actions/goals'
 
 class GoalsContainer extends Component {
-
-    render() {
-      return (
-        <div>
-          <GoalInput addGoal={this.props.addGoal}/>
-          <Goals goals={this.props.goals}/>
-        </div>
-      )
-    }
+  componentDidMount() {
+    this.props.fetchGoals()
   }
-const mapStateToProps = (state) => {
-  return { goals: state.goals }
+
+  render() {
+    return (
+      <div>
+        <GoalInput/>
+        <Goals goals={this.props.goals}/>
+      </div>
+    )
+  }
 }
 
-const mapDispatchToProps = (dispatch) => ({
-   fetchGoals: () => dispatch(fetchGoals()) 
-})
+  const mapStateToProps = (state) => {
+    return { goals: state.goals }
+  }
+
+  const mapDispatchToProps = (dispatch) => ({
+    fetchGoals: () => dispatch(fetchGoals()) 
+  })
 
 export default connect(mapStateToProps, mapDispatchToProps)(GoalsContainer)
