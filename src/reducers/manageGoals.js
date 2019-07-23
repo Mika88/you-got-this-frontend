41e14739846df7f3c22cfa24b925911199ad9b88
Mika88 <mikaspivak@gmail.com>
2019-07-23 15:19:13 -0700

    Remove loading key from state and 'LOADING' action from goalsReducer

--- src/reducers/manageGoals.js
+++ src/reducers/manageGoals.js
@@ -1,22 +1,15 @@
 function goalsReducer(state = {
   goals: [],
-  loading: false }, action) {
+  }, action) {
     switch(action.type) {
-      case 'LOADING':
-      return {
-        ...state, 
-          loading: true
-        }
       case 'FETCH_GOALS':
         return {
           ...state,
-            loading: false,
             goals: action.goals
           }
       case 'ADD_GOAL':
         return {
           ...state,
-          loading: false,
           goals: [...state.goals, action.goal]
         }
       default:
