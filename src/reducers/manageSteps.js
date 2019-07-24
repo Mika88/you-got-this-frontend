b98777bc721e841ece30920b5a995c659d26835b
Mika88 <mikaspivak@gmail.com>
2019-07-24 10:18:36 -0700

    Add FETCH_STEPS case to stepsReducer

--- src/reducers/manageSteps.js
+++ src/reducers/manageSteps.js
@@ -2,7 +2,8 @@ function stepsReducer(state = [], action) {
     switch(action.type) {
       case 'ADD_STEP':
         return [...state, action.step]
-        
+      case 'FETCH_STEPS':
+        return action.steps
       default:
         return state;
       }
