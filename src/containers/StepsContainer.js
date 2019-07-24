217da1f98e99623793fb379652ba34e452115a0e
Mika88 <mikaspivak@gmail.com>
2019-07-24 10:32:14 -0700

    Import fetchSteps function to StepsContainer from actions/steps.js and add a fetchSteps prop in mapDispatchToProps. Add componentDidMount() to StepsContainer to load the fetched steps

--- src/containers/StepsContainer.js
+++ src/containers/StepsContainer.js
@@ -2,28 +2,33 @@ import React, { Component } from 'react';
 import StepInput from '../components/steps/StepInput';
 import { connect } from 'react-redux';
 import Steps from '../components/steps/Steps';
-import { addStep } from '../actions/steps'
+import { addStep, fetchSteps } from '../actions/steps'
 
 class StepsContainer extends Component {
-
-    render() {
-
-      const associatedSteps = this.props.steps.filter( step => step.goal.id === this.props.goalId)
-      
-      return (
-        <div>
-          <StepInput addStep={this.props.addStep} goalId={this.props.goalId}/>
-          <Steps steps={associatedSteps} goalId={this.props.goalId}/>
-        </div>
-      )
-    }
+  componentDidMount() {
+    this.props.fetchSteps()
   }
+
+  render() {
+    
+    const associatedSteps = this.props.steps.filter( step => step.goal.id === this.props.goalId)
+
+    return (
+      <div>
+        <StepInput addStep={this.props.addStep} goalId={this.props.goalId}/>
+        <Steps steps={associatedSteps} goalId={this.props.goalId}/>
+      </div>
+    )
+  }
+}
+
 const mapStateToProps = (state) => {
   return { steps: state.steps }
 }
 
 const mapDispatchToProps = (dispatch) => ({
-   addStep: data => dispatch(addStep(data)) 
+   addStep: data => dispatch(addStep(data)),
+   fetchSteps: () => dispatch(fetchSteps())
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(StepsContainer)
