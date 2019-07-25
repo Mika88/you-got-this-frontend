f1355a50dc366f9a7af4dde7afad074b803bb7aa
Mika88 <mikaspivak@gmail.com>
2019-07-25 14:30:05 -0700

    Add a Route to 'goals/new' in GoalsContainer which renders the GoalInput component. Change map function in Goals component to render links to each goal

--- src/components/goals/Goals.js
+++ src/components/goals/Goals.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Goal from './Goal';
+import { Link } from 'react-router-dom';
 
 const Goals = (props) =>
+    
     <ul>
-        { props.goals.map(goal => <Goal key={goal.id} goal={goal}/>) }
+        { props.goals.map(goal => <li><Link key={goal.id} to={`/goals/${goal.id}`}>{goal.text}</Link></li>) }
     </ul>
         
 export default Goals
--- src/containers/GoalsContainer.js
+++ src/containers/GoalsContainer.js
@@ -13,9 +13,12 @@ class GoalsContainer extends Component {
   render() {
     return (
       <div>
-        <Route exact path="/goals" render={routerProps => <Goals {...routerProps} goals={this.props.goals}/>}>
-          {/* <GoalInput addGoal={this.props.addGoal}/> */}
-        </Route>
+        <Route exact path="/goals" render={routerProps =>  
+          <Goals {...routerProps} goals={this.props.goals}/>  
+         } />
+        <Route exact path="/goals/new"  render={routerProps => 
+          <GoalInput {...routerProps} addGoal={this.props.addGoal}/>
+        } />
       </div>
     )
   }
