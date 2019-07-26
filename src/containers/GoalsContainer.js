1030e258e525d7c6bef5eedc4bf2a799b507c311
Mika88 <mikaspivak@gmail.com>
2019-07-26 15:51:16 -0700

    Uncommnet Route component with path to 'goals/new' to render the GoalsInput form, and clean up code

--- src/containers/GoalsContainer.js
+++ src/containers/GoalsContainer.js
@@ -13,13 +13,11 @@ class GoalsContainer extends Component {
   render() {
     return (
       <div>
-        <Route exact path="/goals" render={routerProps => < Goals {...routerProps} goals={this.props.goals}/> }/>
-        {/* <Route exact path="/goals" render={routerProps =>  
-          <Goals {...routerProps} goals={this.props.goals}/>  
-         } />
+        <Route exact path="/goals" render={routerProps => 
+          < Goals {...routerProps} goals={this.props.goals}/> }/>
         <Route exact path="/goals/new"  render={routerProps => 
           <GoalInput {...routerProps} addGoal={this.props.addGoal}/>
-        } /> */}
+        } /> 
       </div>
     )
   }
