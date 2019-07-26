665ec76e3821b1f156f7ed387558d8eb936438ee
Mika88 <mikaspivak@gmail.com>
2019-07-26 16:48:25 -0700

    Change Goal component into a simple function, and replace the passed argument to match and goals. Define a const variable 'goal' that finds the goal using match.params. Create a condition that handles the async find method and refactor the code to render the goal's information

--- src/components/goals/Goal.js
+++ src/components/goals/Goal.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import StepsContainer from '../../containers/StepsContainer';
 
-const  Goal = (props) => 
-    <div>
-        <li>
-            <h3>{props.goal.text}</h3>
-            <strong>{ props.goal.reason ? "This is important to me because " : ""} {props.goal.reason}</strong>
-            <h4>{ props.goal.deadline ? "Deadline: " : ""} {props.goal.deadline}</h4>
-            <StepsContainer goalId={props.goal.id}/>
-        </li>
-    </div>
-
+function Goal({match, goals}){
+    const goal = goals.find(goal => goal.id == match.params.goalId)
+    if(goal){
+        return (
+        <div>
+            <h3>{goal.text}</h3>
+            <strong>{ goal.reason ? "This is important to me because " : ""} {goal.reason}</strong>
+            <h4>{ goal.deadline ? "Deadline: " : ""} {goal.deadline}</h4>
+            <StepsContainer goalId={goal.id}/>
+        </div>
+        )
+    }else{
+        return <div></div>
+    }
+}
 export default Goal;
