caba68c77acfe9f6d1accf29aba41875efe94eb6
Mika88 <mikaspivak@gmail.com>
2019-07-26 15:47:57 -0700

    Move key variable from Link to <li> element in Goal component

--- src/components/goals/Goals.js
+++ src/components/goals/Goals.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Goals = (props) =>
     
     <ul>
-        { props.goals.map(goal => <li><Link key={goal.id} to={`/goals/${goal.id}`}>{goal.text}</Link></li>) }
+        { props.goals.map(goal => <li key={goal.id}><Link to={`/goals/${goal.id}`}>{goal.text}</Link></li>) }
     </ul>
         
 export default Goals
