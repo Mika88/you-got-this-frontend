024a14a8f0098382cc96840df158033e60892359
Mika88 <mikaspivak@gmail.com>
2019-07-27 12:54:39 -0700

    Install react-moment and react-add-to-calender packages and import it in EventInput component, add 'AddToCalender' component

--- src/components/events/EventInput.js
+++ src/components/events/EventInput.js
@@ -1,22 +1,37 @@
 import React, { Component } from 'react';
 import DateTimePicker from 'react-datetime-picker';
-
+import AddToCalendar from 'react-add-to-calendar';
+import 'react-add-to-calendar/dist/react-add-to-calendar.css'
 export default class EventInput extends Component {
     state={
         done: false,
-        date: new Date()
+        date: new Date(),
+        event: {
+            title: 'Sample Event',
+            description: 'This is the sample event provided as an example only',
+            location: 'Portland, OR',
+            startTime: '2016-09-16T20:15:00-04:00',
+            endTime: '2016-09-16T21:45:00-04:00'
+          }
     }
 
-    onChange = date => this.setState({ date }) 
-
+    onChange = date => this.setState({ date }, () => console.log(this.state))  
+    
     render() {
+        let icon = { 'calendar-plus-o': 'left' };
         return (
            <div>
+               <span>
              <DateTimePicker
                 onChange={this.onChange}
                 value={this.state.date}
               />
-           </div> 
+              <AddToCalendar 
+                event={this.state.event}
+                buttonTemplate={icon}
+                />
+                </span>
+           </div>
         )
     }
 }
