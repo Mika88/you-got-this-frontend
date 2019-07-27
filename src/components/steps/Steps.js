582a483efd875e0757ec2618ea18569b6ad71f48
Mika88 <mikaspivak@gmail.com>
2019-07-27 10:20:26 -0700

    Change argument passed to Steps function in Steps component to 'steps'

--- src/components/steps/Steps.js
+++ src/components/steps/Steps.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Step from './Step';
 
-function Steps(props){
+function Steps({steps}){
   return (
   <div>
-    <h4>{props.steps.length > 0 ? "Steps: " : ""}</h4>
+    <h4>{steps.length > 0 ? "Steps: " : ""}</h4>
       <ol>
-        { props.steps.map(step => <Step key={step.id} step={step}/>) }
+        { steps.map(step => <Step key={step.id} step={step}/>) }
       </ol>
   </div>
   )
