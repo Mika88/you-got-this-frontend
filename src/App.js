53ffef9cb5f1220975ed3b22bba09ae9f86837bb
Mika88 <mikaspivak@gmail.com>
2019-07-30 15:31:57 -0700

    Add a backgroud image to App.css

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import React from 'react';
 import GoalsContainer from './containers/GoalsContainer';
 import { Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
+import './App.css';
 
 class App extends React.Component {
   render() {
     return (
+    <div className="bg">
       <div className="container">
          <Navigation/>
          <br /><br />
         <Route path="/goals" render={routerProps => <GoalsContainer {...routerProps}/>}/>
         <Route exact path="/" render={() => <h1>Home</h1>}/>
       </div>
+    </div>
     );
   }
 
 }
 
 export default App;
+
