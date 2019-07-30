83b874ec45aec2b81ae332842710e0d51f868a8b
Mika88 <mikaspivak@gmail.com>
2019-07-30 11:15:11 -0700

    Create Navbar component, import Navlink and add a link to '/' - Home. Import Navbar component in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import GoalsContainer from './containers/GoalsContainer';
 import { Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Navbar/>
         <Route path="/goals" render={routerProps => <GoalsContainer {...routerProps}/>}/>
         <Route exact path="/" render={() => <h1>Home</h1>}/>
       </div>
