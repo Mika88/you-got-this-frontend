f17b61074e44552d2a7bc6a3548b6d09a2806a49
Mika88 <mikaspivak@gmail.com>
2019-07-31 08:58:13 -0700

    Add a logo image to src directory and import it in Navigation component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import Logo from '../logo.jpg'
 
 class Navigation extends React.Component {
     render() {
       return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <img src={Logo} alt='logo' />
           <NavLink 
             to="/"
             className="navbar-brand" 
