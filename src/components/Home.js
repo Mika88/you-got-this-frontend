892f8a452ff3881d7ca617ebd7adb20d47286366
Mika88 <mikaspivak@gmail.com>
2019-07-31 15:07:07 -0700

    Add user instructions to Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,8 +7,20 @@ function Home() {
     <div> 
         <div className="component">
             <h1>Welcome to You Got This!</h1>
-            {/* <h4>A Goal tracker application</h4> */}
-            <img src={Quote} alt='quote' style={{height:'100px'}}/>
+            <h4>Set yourself up for success with our goal tracker:</h4>
+            <strong>
+                <ol>
+                    <li>Create a goal</li>
+                    <li>Click it</li>
+                    <li>Add steps</li>
+                    <li>For each step create events</li>
+                    <li>Add it to your calendar (optional)</li>
+                    <li>Submit it</li>
+                    <li>Once you finished an event check it off</li>
+                    <li>and remember:</li>
+                    <img src={Quote} alt='quote' style={{height:'100px'}}/>
+                </ol>
+            </strong>
         </div>
     </div>
      )
