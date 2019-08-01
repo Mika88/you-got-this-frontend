5da5386ea82173f4045f5569c0df2fc05ff42cd6
Mika88 <mikaspivak@gmail.com>
2019-08-01 11:34:01 -0700

    Add a key to div element in map function in Goals component

--- src/components/goals/Goals.js
+++ src/components/goals/Goals.js
@@ -5,27 +5,27 @@ import '../../App.css';
 function Goals(props){
   
   const {goals, match, deleteGoal} = props
-  console.log(goals)
+ 
   if(goals.length > 0){
-  return (
-    <div className="component">
-        { goals.map(goal => 
-        <div>
-           <div className="d-inline-block">
-              <button 
-                className="btn btn-link btn-xs" 
-                onClick={() => 
-                  deleteGoal({text: goal.text, about: goal.about, deadline: goal.deadline}, goal.id)}>X
-              </button>
-           </div>
-           <div className="d-inline-block">
-              <h4><Link to={`${match.url}/${goal.id}`}> {goal.text}</Link></h4>
-           </div>
-           <br /><br />
-        </div>
-        )}
-    </div>
-   )
+    return (
+      <div className="component">
+          { goals.map(goal => 
+          <div key={goal.id}>
+            <div className="d-inline-block">
+                <button 
+                  className="btn btn-link btn-xs" 
+                  onClick={() => 
+                    deleteGoal({text: goal.text, about: goal.about, deadline: goal.deadline}, goal.id)}>X
+                </button>
+            </div>
+            <div className="d-inline-block">
+                <h4><Link to={`${match.url}/${goal.id}`}> {goal.text}</Link></h4>
+            </div>
+            <br /><br />
+          </div>
+          )}
+      </div>
+    )
   }else{
     return(
       <div className="component">
