bac5870c9a965611e4d7ad726ee02ca477eb0a62
Mika88 <mikaspivak@gmail.com>
2019-11-04 08:57:16 -0800

    Create a UsersContainer.js file and define a UsersContainer class with basic render method

---
+++ src/containers/UsersContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class UsersContainer extends Component {
+  render(){
+    return(
+      <div>
+      </div>
+    )
+  }
+}
+
+export default UsersContainer;
